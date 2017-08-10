5836cbd15e63c35b8deadd9839104f9515cb1245
Louise Milligan <louise.milligan@hotmail.co.uk>
2017-08-10 17:18:14 +0100

    Finished Lab

--- src/main/java/io/zipcode/microlabs/loops/Loops.java
+++ src/main/java/io/zipcode/microlabs/loops/Loops.java
@@ -1,5 +1,8 @@
 package io.zipcode.microlabs.loops;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Loops {
 
@@ -11,4 +14,115 @@ public class Loops {
         return response;
     }
 
+    
+    public static String lastElement(String[] input){
+        String response = "*** Output ***\n";
+        String output = input[input.length-1];
+        response += output;
+        
+        return response;
+    }
+    
+    public static String secondLastElement(String[] input){
+        String response = "*** Output ***\n";
+        String output = input[input.length-2];
+        response += output;
+        
+        return response;
+    }
+    
+    public static String reverseArray(String[] input){
+        String response = "*** Output ***";
+       
+        List<String> list = Arrays.asList(input);
+        Collections.reverse(list);
+        input = (String[])list.toArray();
+        
+        for(int x=0; x< input.length; x++){
+            response +="\n" + input[x];
+       
+        }
+        return response;
+    }
+        
+    public static String reverseArray2(String[] input){
+            String response = "*** Output ***";
+            int position = 0;
+            String reverse[] = new String[input.length];
+            
+            for (int x= input.length-1; x>=0; x--) {
+            	reverse[position]= input[x];
+            	position++;
+            }
+            	
+            for(int x=0; x< input.length; x++){
+            response += "\n" + reverse[x];
+            }
+            return response;
+            	
+            }
+    
+    
+    public static String palindrome(String[] input, String [] palindromeInput){
+    	// A loop that checks if the 1st and so on variable of 
+    	String test[] = new String[input.length];
+    	String response = "*** Output ***";
+    	for (int x=0; x<input.length; x++){
+    		if (input[x] == palindromeInput[x])
+    		{
+    			test[x]= "True";
+    		}
+    		else 
+    		{
+    			test[x]= "False";
+    		}
+    	}
+    	
+    	for(int x=0; x< test.length; x++){
+            response += "\n" + test[x];
+            }
+    	
+            return response;
+    }
+    
+    public static String conDuplicates(int[] input){
+    	String output = "*** Output ***";
+    	String response= output + "\n" + String.valueOf(input[0]);
+    	for (int x=1; x<=input.length-1; x++){
+    			if (input[x-1]!=input[x]) {
+    				response += "\n" + input[x];
+    			}
+    	}
+    	return response;
+    }
+    
+    public static String packDuplicates(char[] input)
+    {
+    	String output = "*** Output ***";
+    	String response = output + "\n" + input[0];
+    	for (int x=1; x<=input.length-1; x++)
+    	{
+    	if(input[x-1]!=input[x])
+    	{
+    		response += ", ";
+    	}
+    	response += input[x];
+    		
+    	}
+    	return response;
+    }
+    	
+
 }
+
+    	
+   
+    	
+    				
+    				
+    			
+    
+   
+     
+        
+    
--- src/test/java/io/zipcode/microlabs/loops/LoopsTest.java
+++ src/test/java/io/zipcode/microlabs/loops/LoopsTest.java
@@ -24,4 +24,114 @@ public class LoopsTest {
         //:Then
         Assert.assertEquals(expected, actual);
     }
+    
+    @Test
+    public void secondLastElement(){
+        // :Given
+        String [] breakfast = {"Sausage", "Eggs", "Beans",
+                "Bacon", "Tomatoes", "Mushrooms"};
+        String expected = "*** Output ***\nTomatoes";
+        //:when
+        String actual =Loops.secondLastElement(breakfast);
+
+        //:Then
+        Assert.assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void reverseArray(){
+        // :Given
+    	String [] breakfast = {"Sausage", "Eggs", "Beans",
+                "Bacon", "Tomatoes", "Mushrooms"};
+        String expected = "*** Output ***\n"
+        		+"Mushrooms\n" +
+                "Tomatoes\n" +
+                "Bacon\n" +
+                "Beans\n" +
+                "Eggs\n" +
+                "Sausage";
+        //:when
+        String actual =Loops.reverseArray(breakfast);
+
+        //:Then
+        Assert.assertEquals(expected, actual);
+    }
+   
+    
+    @Test
+    public void reverseArray2(){
+        // :Given
+    	String [] breakfast = {"Sausage", "Eggs", "Beans",
+                "Bacon", "Tomatoes", "Mushrooms"};
+        String expected = "*** Output ***\n"
+        		+"Mushrooms\n" +
+                "Tomatoes\n" +
+                "Bacon\n" +
+                "Beans\n" +
+                "Eggs\n" +
+                "Sausage";
+        //:when
+        String actual = Loops.reverseArray2(breakfast);
+
+        //:Then
+        Assert.assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void palindrome(){
+        // :Given
+    	  String [] palindrome = {"Sausage", "Eggs", "Beans",
+                  "Bacon", "Tomatoes", "Mushrooms"};
+    	String [] breakfast = {"Sausage", "Eggs", "Beans",
+                "Bacon", "Tom", "Mushrooms"};
+        String expected = "*** Output ***\n"
+        		+"True\n" +
+                "True\n" +
+                "True\n" +
+                "True\n" +
+                "False\n" +
+                "True";
+        //:when
+        String actual = Loops.palindrome(breakfast, palindrome);
+
+        //:Then
+        Assert.assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void conDuplicates()
+    {
+        // :Given
+    	int [] nums = {1,1,3,3,3,2,2,2,1,1,1,1,4,4,4,4};
+    	
+        String expected = "*** Output ***\n"
+        		+"1\n" +
+                "3\n" +
+                "2\n" +
+                "1\n" +
+                "4";
+        //:when
+        String actual = Loops.conDuplicates(nums);
+
+        //:Then
+        Assert.assertEquals(expected, actual);
+    }
+        
+        
+     @Test
+     public void packDuplicates()
+     {
+            // :Given
+    	 char [] letters = {'a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e'};
+        	
+            String expected = "*** Output ***\naaaa, b, cc, aa, d, eeee"; 
+            //:when
+            String actual = Loops.packDuplicates(letters);
+
+            //:Then
+            Assert.assertEquals(expected, actual);
+    
+    
+     }
 }
+
